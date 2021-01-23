0e19bbe30a5dcf52de56ee46930311c32d99c695
fitra <cahrizki.rf@gmail.com>
2021-01-24 06:30:14 +0700

    onPress navigate to slug

--- src/components/Notif/NotifList.js
+++ src/components/Notif/NotifList.js
@@ -1,19 +1,19 @@
 import React from 'react';
-import {View, FlatList, StyleSheet, Text} from 'react-native';
-import {useQuery, gql} from '@apollo/client';
-import {dateFormat} from '../Helper'
+import {View, FlatList, StyleSheet, Text, TouchableOpacity} from 'react-native';
+import {useQuery, useMutation, gql} from '@apollo/client';
+import {dateFormat} from '../Helper';
 import Loading from '../Loading';
 import Error from '../Error';
+import {useNavigation} from '@react-navigation/native';
 
 const GET_ACTIVITIES = gql`
   query Get_Activities($id: ID!) {
     user(id: $id) {
       student {
-        notifications {
+        notifications(where: {terbaca: false}) {
           id
           notifikasi
           waktu
-          terbaca
           slug
         }
       }
@@ -21,20 +21,55 @@ const GET_ACTIVITIES = gql`
   }
 `;
 
-const Item = ({title, date,terbaca}) => (
-  <View style={styles.container}>
-    <Text style={{color: '#71717A'}}>{title}</Text>
-    <Text style={{color: '#A1A1AA', fontSize:11}}>{dateFormat(date)}</Text>
-  </View>
-);
+const SET_TERBACA = gql`
+  mutation Set_Terbaca($id: ID!) {
+    updateNotification(input: {where: {id: $id}, data: {terbaca: true}}) {
+      notification {
+        id
+        notifikasi
+        terbaca
+      }
+    }
+  }
+`;
 
-const renderItem = ({item}) => (
-  <Item key={item.id} title={item.notifikasi} terbaca={item.terbaca} date={item.waktu} />
-);
+const Item = (Props) => {
+  const navigation = useNavigation();
+  const {id, notifikasi, waktu, slug} = Props.data;
+  console.log(Props.data)
+  const [Set_Terbaca, {data}] = useMutation(SET_TERBACA);
+
+  // ketika user klik diarahkan ke screen sesuai slug
+  const handlePress = async (id) => {
+    try {
+      const dt = await Set_Terbaca({
+        variables: {id: id},
+      });
+      navigation.replace(slug);
+    } catch (e) {
+      alert('Upsss terjadi masalah');
+      console.log(e);
+    }
+  };
+
+  return (
+    <TouchableOpacity
+      onPress={() => {
+        handlePress(id);
+      }}
+      style={styles.container}>
+      <Text style={{color: '#71717A'}}>{notifikasi}</Text>
+      <Text style={{color: '#A1A1AA', fontSize: 11}}>{dateFormat(waktu)}</Text>
+    </TouchableOpacity>
+  );
+};
+
+const renderItem = ({item}) => <Item key={item.id} data={item} />;
 
 const AktivitasList = () => {
   const {loading, error, data} = useQuery(GET_ACTIVITIES, {
-    variables: {id: '2'},pollInterval:500
+    variables: {id: '2'},
+    pollInterval: 500,
   });
 
   if (loading) return <Loading />;
@@ -59,7 +94,8 @@ const AktivitasList = () => {
                 borderRadius: 8,
                 justifyContent: 'center',
               }}>
-              <Text style={{fontSize: 15, color: '#71717A', textAlign: 'center'}}>
+              <Text
+                style={{fontSize: 15, color: '#71717A', textAlign: 'center'}}>
                 Tidak ada Data
               </Text>
             </View>
@@ -80,7 +116,7 @@ const styles = StyleSheet.create({
     paddingHorizontal: 25,
     paddingVertical: 20,
     backgroundColor: '#F4F4F5',
-    borderRadius: 9
+    borderRadius: 9,
   },
 });
 
